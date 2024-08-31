deb15f77d8ea94cb9a3ad3dcd012f75353170d87
Satviki <satviki.sharma13@gmail.com>
2024-08-30 23:05:43 -0500

    setup of data

---
+++ EconometricAnal_StockMarket/setup.py
@@ -0,0 +1,15 @@
+import numpy as np
+import pandas as pd
+import statsmodels.api as sm # type: ignore
+from statsmodels.tsa.stattools import adfuller, coint # type: ignore
+from statsmodels.tsa.api import VAR
+from statsmodels.tsa.ar_model import AutoReg
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+# Load the data (Assuming the dataset is in CSV format)
+data = pd.read_csv('stockMarket_data.csv', parse_dates=['Date'], index_col='Date')
+
+# Separate variables
+stock_prices = data['Stock_Prices']
+investor_sentiment = data['Investor_Sentiment']
